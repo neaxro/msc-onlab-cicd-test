e4bc845043d207acd02829f39c8c3a6147d48d8b
Axel Roland Nemes <neaxro@gmail.com>
2024-05-06 20:45:51 +0200

    Create dummy tests for poc

--- Makefile
+++ Makefile
@@ -1,8 +1,18 @@
 packages:
-	pip install -r requirements.txt
+	pip install -r api/requirements.txt
+
+build_test:
+	docker build -t local-testing:latest .
 
 build:
-	docker build -t local-testing .
+	docker build -t msc-onlab-backend:latest .
+
+venv:
+	# Start virtual environment
+	. api/.venv/bin/activate
+
+test: venv
+	pytest
 
 up:
 	# Start virtual environment
---
+++ api/test/test_login.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_restful import Api
+import pytest
+
+@pytest.fixture()
+def app():
+    app = Flask(__name__)
+    api = Api(app)
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+# --------------------------------------------
+# |                 TESTS                    |
+# --------------------------------------------
+
+def test_dummy_1(client):
+    sum = 1 + 1
+    assert sum == 2
+
+def test_dummy_2(client):
+    sum = 1 + 2
+    assert sum > 2
