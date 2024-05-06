0ac6501db322d61e2ad843c532a3d9b21a6bc3bc
Axel Roland Nemes <neaxro@gmail.com>
2024-05-06 21:24:28 +0200

    Make some backend modification for test

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -9,3 +9,5 @@ from app.auth.register.register_routes import RegisterResource
 from app.user.user_routes import UserResource
 from app.household.household_routes import HouseholdResource
 from app.household.tasks.task_routes import TaskResource
+
+# Modification
